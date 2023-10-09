05b9729f10563f555c6d9a0d681ac68f2be953ca
Jason Zhao <jason.zhao@wustl.edu>
2023-10-08 22:02:11 -0500

    Project 1 finished Q8 by Jason Zhao and Tovi Tu

--- project1-search/searchAgents.py
+++ project1-search/searchAgents.py
@@ -667,7 +667,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.astar(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -704,7 +704,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 
 def mazeDistance(point1, point2, gameState):
